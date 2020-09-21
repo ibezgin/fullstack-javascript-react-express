444e57b57e502a9feda0dfd5a87dd1db5c594719
ibezgin <kurt1v@yandex.ru>
2020-09-21 22:49:36 +0300

    no changes

--- src/client/components/Header.tsx
+++ src/client/components/Header.tsx
@@ -3,21 +3,21 @@ import { createStyles, Theme } from "@material-ui/core/styles";
 import React from "react";
 
 const useStyles = makeStyles((theme: Theme) =>
-  createStyles({
-    appBar: {
-      zIndex: theme.zIndex.drawer + 1,
-    },
-  }),
+    createStyles({
+        appBar: {
+            zIndex: theme.zIndex.drawer + 1,
+        },
+    }),
 );
 export const Header: React.FunctionComponent = () => {
-  const classes = useStyles({});
-  return (
-    <AppBar position='fixed' className={classes.appBar}>
-      <Toolbar>
-        <Typography variant='h6' noWrap>
-          Fullstack TypeScript
-        </Typography>
-      </Toolbar>
-    </AppBar>
-  );
+    const classes = useStyles({});
+    return (
+        <AppBar position="fixed" className={classes.appBar}>
+            <Toolbar>
+                <Typography variant="h6" noWrap>
+                    Fullstack TypeScript
+                </Typography>
+            </Toolbar>
+        </AppBar>
+    );
 };
