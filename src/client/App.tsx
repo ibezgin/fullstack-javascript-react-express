d903ebf460dd446ed2d69ecd057475c2f58fda79
ibezgin <kurt1v@yandex.ru>
2020-09-29 22:01:19 +0300

    change gql client conf

--- src/client/App.tsx
+++ src/client/App.tsx
@@ -4,7 +4,7 @@ import { RootView } from "./view";
 import { ApolloProvider, ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const App = () => {
-    console.log(window.location);
+    // console.log(window.location);
     const client = new ApolloClient({
         uri: `${window.location.protocol}//${window.location.host}/graphql`,
         cache: new InMemoryCache(),
